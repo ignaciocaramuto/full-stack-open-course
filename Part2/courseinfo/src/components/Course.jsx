5f7db4b939e3cb1fb2230b4c1f74cc1e921852ec
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-08-05 18:48:23 +0200

    exercise 2.1 Course information step 6

---
+++ Part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,28 @@
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
