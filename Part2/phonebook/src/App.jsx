448aa42cc8b155299c4e72d37815791fc3796f2e
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-08-06 18:25:54 +0200

    Exercise 2.12 The Phonebook step 7

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -28,9 +28,11 @@ const App = () => {
       name: newName,
       number: newNumber,
     };
-    setPersons(persons.concat(newPerson));
-    setNewName("");
-    setNewNumber("");
+    axios.post("http://localhost:3001/persons", newPerson).then((response) => {
+      setPersons(persons.concat(response.data));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   const handleFilterChange = (event) => {
