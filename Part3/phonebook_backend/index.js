36764fec7e84faa9babead74be2af6ca32c8ca49
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-08-21 20:01:31 +0200

    Part 3 Exercises 3.1 to 3.8

---
+++ Part3/phonebook_backend/index.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+let phonebook = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000) + 1;
+};
+
+app.get("/info", (request, response) => {
+  response.send(
+    "<p>Phonebook has info for " +
+      phonebook.length +
+      " people</p><p>The current time is " +
+      new Date() +
+      "</p>"
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const { id } = request.params;
+  const person = phonebook.find((person) => person.id === id);
+  if (!person) {
+    return response.status(404).end();
+  }
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const { id } = request.params;
+  phonebook = phonebook.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+  if (!name || !number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = phonebook.find((person) => person.name === name);
+
+  if (person) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name,
+    number,
+  };
+
+  phonebook = phonebook.concat(newPerson);
+
+  response.status(201).json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
