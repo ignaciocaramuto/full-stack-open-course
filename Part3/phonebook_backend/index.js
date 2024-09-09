a6b5efa5158fcf2e60cede9e0b17a8eeacc9ff14
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-09-09 21:19:34 +0200

    Exercises 3.15-3.18

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -27,26 +27,36 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then((person) => {
-    response.json(person);
-  });
+  Person.find({})
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const { id } = request.params;
-  Person.findById(id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   const { id } = request.params;
-  Person.findByIdAndDelete(id).then(() => {
-    response.status(204).end();
-  });
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = request.body;
   if (!name || !number) {
     return response.status(400).json({
@@ -54,15 +64,49 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  Person.findOne({ name: name }).then((person) => {
+    if (person) {
+      return response.status(400).json({
+        error: "name must be unique",
+      });
+    }
+  });
+
   const newPerson = new Person({
     name,
     number,
   });
 
-  newPerson.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  newPerson
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  const { id } = request.params;
+
+  Person.findByIdAndUpdate(id, { name, number }, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
