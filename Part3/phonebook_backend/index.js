32ec3641c5b0af8d53edc82510319305cb5947cf
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-09-09 21:51:13 +0200

    Exercises 3.19-3.21

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -26,7 +26,7 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({})
     .then((person) => {
       response.json(person);
@@ -47,7 +47,7 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const { id } = request.params;
   Person.findByIdAndDelete(id)
     .then(() => {
@@ -89,7 +89,11 @@ app.put("/api/persons/:id", (request, response, next) => {
   const { name, number } = request.body;
   const { id } = request.params;
 
-  Person.findByIdAndUpdate(id, { name, number }, { new: true })
+  Person.findByIdAndUpdate(
+    id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -101,6 +105,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
