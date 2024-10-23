da36f6d12d5b9d88f2d199f636fb384478395cf3
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-10-23 19:03:10 +0200

    Exercises 4.8 - 4.12

--- Part4/blog-app/controllers/blogs.js
+++ Part4/blog-app/controllers/blogs.js
@@ -1,18 +1,20 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
+blogsRouter.get('', async (request, response) => {
+    const blogs = await Blog.find({});
+    response.json(blogs)
 })
 
-blogsRouter.post('', (request, response) => {
+blogsRouter.post('', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog.save().then(result => {
-        response.status(201).json(result)
-    })
+    if (!blog.title || !blog.url || !blog.author) {
+        return response.status(400).json({ error: 'title, author, and url are required' })
+    }
+
+    const result = await blog.save();
+    response.status(201).json(result)
 })
 
 module.exports = blogsRouter
---
+++ Part4/blog-app/tests/blog_api.test.js
@@ -0,0 +1,79 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const { MONGODB_URI } = require('../utils/config')
+const { initialBlogs, nonExistingId, blogsInDb } = require('./test_helper.js')
+
+beforeEach(async () => {  
+    await mongoose.connect(MONGODB_URI)
+    await Blog.deleteMany({})
+    for (const blog of initialBlogs) {
+        const blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+    const blogsAtEnd = await blogsInDb()
+  
+    assert.strictEqual(blogsAtEnd.length, initialBlogs.length)
+})
+  
+test('the first blog is about react patterns', async () => {
+    const blogsAtEnd = await blogsInDb()
+
+    const contents = blogsAtEnd.map(e => e.title)
+    assert(contents.includes('React patterns'))
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+    }
+
+    await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('Canonical string reduction'))
+})
+
+test('blog without title is not added', async () => {
+    const newBlog = {
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await blogsInDb()
+  
+    assert.strictEqual(blogsAtEnd.length, initialBlogs.length)
+  })
+
+after(async () => {
+  await mongoose.connection.close()
+})
