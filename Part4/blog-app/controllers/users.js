3c55042cf0dcce8c689f67b333565a00682b79a3
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-11-09 12:36:56 +0100

    Tabulated code

--- Part4/blog-app/controllers/users.js
+++ Part4/blog-app/controllers/users.js
@@ -3,47 +3,47 @@ const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
 userRouter.get('', async (request, response) => {
-    const users = await User.find({})
-    response.json(users)
+  const users = await User.find({})
+  response.json(users)
 })
 
 userRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+  const { username, name, password } = request.body
 
-    if (!username ) {
-      return response.status(400).json({ error: 'username missing' })
-    }
+  if (!username ) {
+    return response.status(400).json({ error: 'username missing' })
+  }
 
-    if (!password ) {
-      return response.status(400).json({ error: 'password missing' })
-    }
+  if (!password ) {
+    return response.status(400).json({ error: 'password missing' })
+  }
 
-    if (username.length < 3) {
-      return response.status(400).json({ error: 'username must be at least 3 characters long' })
-    }
+  if (username.length < 3) {
+    return response.status(400).json({ error: 'username must be at least 3 characters long' })
+  }
 
-    if (password.length < 3) {
-      return response.status(400).json({ error: 'password must be at least 3 characters long' })
-    }
+  if (password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+  }
 
-    const existingUser = await User.findOne({ username })
+  const existingUser = await User.findOne({ username })
 
-    if (existingUser) {
-      return response.status(400).json({ error: `${username} already exists` })
-    }
-  
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-  
-    const user = new User({
-      username,
-      name,
-      passwordHash,
-    })
-  
-    const savedUser = await user.save()
-  
-    response.status(201).json(savedUser)
+  if (existingUser) {
+    return response.status(400).json({ error: `${username} already exists` })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
 })
 
 module.exports = userRouter
