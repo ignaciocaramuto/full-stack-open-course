0bab7f98d32ebe48daea7c615af32132f575c22a
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-11-09 13:29:25 +0100

    Exercise 4.18 - 4.19

--- Part4/blog-app/controllers/blogs.js
+++ Part4/blog-app/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user.js')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1});
@@ -8,13 +17,19 @@ blogsRouter.get('', async (request, response) => {
 })
 
 blogsRouter.post('', async (request, response) => {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
     const { title, url, author, likes } = request.body
 
     if (!title || !url || !author) {
         return response.status(400).json({ error: 'title, author, and url are required' })
     }
-
-    const user = (await User.find({})).at(0)
 
     const blog = new Blog({
         title,
