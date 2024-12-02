af3090b897c55ec28f64d5763fe744503bb20f1d
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-12-02 12:18:49 -0300

    Exercise 5.7

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+    <div style={hideWhenVisible}>
+      <span>{blog.title} {blog.author}</span>
+        <button onClick={toggleVisibility}>{ visible ? 'hide' : 'view' }</button>
+    </div>
+    <div style={showWhenVisible}>
+      <span>{ blog.url }</span>
+      <span>{ blog.likes }</span>
+      <span>{ blog.author }</span>
+    </div>
+  </div>
+  )
+}
 
 export default Blog
