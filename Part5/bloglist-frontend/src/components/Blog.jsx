da36129c4b165691fc8c20791fd8eb128a34fdee
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-12-17 11:53:58 -0300

    Exercise 5.7 fix import + hide functionality

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false)
@@ -19,16 +20,17 @@ const Blog = ({ blog }) => {
 
   return (
     <div style={blogStyle}>
-    <div style={hideWhenVisible}>
-      <span>{blog.title} {blog.author}</span>
-        <button onClick={toggleVisibility}>{ visible ? 'hide' : 'view' }</button>
+      <div style={hideWhenVisible}>
+        <span>{blog.title} {blog.author}</span>
+          <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{ blog.url }</p>
+        <p>likes: { blog.likes }</p>
+        <p>{ blog.author }</p>
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
     </div>
-    <div style={showWhenVisible}>
-      <span>{ blog.url }</span>
-      <span>{ blog.likes }</span>
-      <span>{ blog.author }</span>
-    </div>
-  </div>
   )
 }
 
